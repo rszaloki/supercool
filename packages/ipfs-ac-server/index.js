369b6a99dfb2805f03229b28c28c98e934bcb7b9
robi <robert.szaloki@euedge.com>
2018-10-03 16:50:45 +0200

    use normal addresses

--- packages/ipfs-ac-server/index.js
+++ packages/ipfs-ac-server/index.js
@@ -10,7 +10,11 @@ const ipfs = new IPFS({
     pubsub: true
   },
   config: {
-    Bootstrap: ['/ip4/127.0.0.1/tcp/9999/ws/ipfs/QmQRygvS2PAbk5yKEft8f8HryJr8WoEKp82gctE1NvNR6X']
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
   }
 })
 
